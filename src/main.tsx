510b91b532e2d3ae692bff3baa094e880a207521
Andrew Demydenko <and.demydenko@gmail.com>
2025-06-12 12:39:13 +0300

    base setup react-router, tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import ProjectsPage from "@/pages/ProjectsPage.tsx";
 import ProjectDetailsPage from "@/pages/ProjectDetailsPage.tsx";
-import "./index.css";
+import HomePage from "@/pages/HomePage";
+import "@/assets/styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
+        <Route index element={<HomePage />} />
 
         <Route path="projects">
           <Route index element={<ProjectsPage />} />
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="bg-black text-white">Home Page</div>;
+}
