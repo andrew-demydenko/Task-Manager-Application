2af4d8aa499a16b215f9ccd42b9e34f2e553ec94
Andrew Demydenko <and.demydenko@gmail.com>
2025-06-15 22:45:19 +0300

    add 404 for redirects

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        404: "./404.html",
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
